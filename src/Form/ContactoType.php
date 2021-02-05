81d38756ff57ccc8837ae2f7e131ce322c47e1a3
carlos <carlosjt@gmail.com>
2021-02-05 13:19:07 +0100

    Edit

---
+++ src/Form/ContactoType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Contacto;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactoType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nombre')
+            ->add('apellido')
+            ->add('correo')
+            ->add('telefono')
+            ->add('tipo')
+            ->add('notas')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Contacto::class,
+        ]);
+    }
+}
